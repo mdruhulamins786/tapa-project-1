25de11747448c397e6873862dea24124e21f8de3
Md Ruhul Amin <99403327+mdruhulamins786@users.noreply.github.com>
2025-02-05 00:16:03 +0600

    data fecth

--- my-project/src/components/cars_board/cars_list/CarsList.jsx
+++ my-project/src/components/cars_board/cars_list/CarsList.jsx
@@ -1,36 +1,13 @@
 import BuyButton from "./Buy_Button/BuyButton";
-import Card from "./card/Card";
 
 const CarsList = ({ show, search }) => {
   return (
     <>
-      {show ? (
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
-          {search.map((car) => {
-            const { title, brand, year, price, isPremium } = car;
-            return (
-              <div className="" key={car.id}>
-                {car.isPremium && (
-                  <div className="border rounded-md p-5">
-                    <p className="text-orange-500 text-xl font-semibold mb-2">
-                      {title}
-                    </p>
-                    <p>Brand: {brand}</p>
-                    <p>Year: {year}</p>
-                    <p>Price: {price}</p>
-                    <p>Premium: {isPremium ? "Yes" : "No"}</p>
-                    <BuyButton>Buy Now</BuyButton>
-                  </div>
-                )}
-              </div>
-            );
-          })}
-        </div>
-      ) : (
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
-          {search.map((car) => {
-            const { title, brand, year, price, isPremium } = car;
-            return (
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+        {search.map((car) => {
+          const { title, brand, year, price, isPremium } = car;
+          return (
+            <div className="" key={car.id}>
               <div className="border rounded-md p-5">
                 <p className="text-orange-500 text-xl font-semibold mb-2">
                   {title}
@@ -41,10 +18,10 @@ const CarsList = ({ show, search }) => {
                 <p>Premium: {isPremium ? "Yes" : "No"}</p>
                 <BuyButton>Buy Now</BuyButton>
               </div>
-            );
-          })}
-        </div>
-      )}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
